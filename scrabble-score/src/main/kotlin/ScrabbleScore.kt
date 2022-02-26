6fc43219b529b9e72e26e96add67b28a329554a7
Mark Standen <mark@markstanden.dev>
2022-02-26 09:47:44 +0000

    completed scrabble score

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,10 +1,28 @@
-object ScrabbleScore {
+/**
+ * Extension method for List to convert a list of entries to a Map with a shared value.
+ */
+fun <K, V> List<K>.toMap(hasValue: V): Map<K, V> {
+    return this.associateBy({ it }, { hasValue })
+}
 
-    fun scoreLetter(c: Char): Int {
-        TODO("Implement this function to complete the task")
+fun scrabbleTileValues(): Map<Char, Int> {
+    return HashMap<Char, Int>().plus(listOf('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T').toMap(1))
+        .plus(listOf('D', 'G').toMap(2))
+        .plus(listOf('B', 'C', 'M', 'P').toMap(3))
+        .plus(listOf('F', 'H', 'V', 'W', 'Y').toMap(4))
+        .plus(listOf('K').toMap(5))
+        .plus(listOf('J', 'X').toMap(8))
+        .plus(listOf('Q', 'Z').toMap(10))
+}
+
+object ScrabbleScore {
+    private val letterValues = scrabbleTileValues()
+
+    private fun scoreLetter(c: Char): Int {
+        return letterValues[c] ?: 0;
     }
 
     fun scoreWord(word: String): Int {
-        TODO("Implement this function to complete the task")
+        return word.uppercase().sumOf { scoreLetter(it) }
     }
 }
