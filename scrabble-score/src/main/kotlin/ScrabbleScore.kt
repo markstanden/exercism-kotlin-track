3ac82658953ecf2ef32ebb82e4cf4be5ed0bd26b
Mark Standen <mark@markstanden.dev>
2022-02-26 17:17:33 +0000

    added tests

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,18 +1,18 @@
 /**
  * Extension method for List to convert a list of entries to a Map with a shared value.
  */
-fun <K, V> List<K>.toMap(hasValue: V): Map<K, V> {
+fun String.toMap(hasValue: Int): Map<Char, Int> {
     return this.associateBy({ it }, { hasValue })
 }
 
 fun scrabbleTileValues(): Map<Char, Int> {
-    return HashMap<Char, Int>().plus(listOf('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T').toMap(1))
-        .plus(listOf('D', 'G').toMap(2))
-        .plus(listOf('B', 'C', 'M', 'P').toMap(3))
-        .plus(listOf('F', 'H', 'V', 'W', 'Y').toMap(4))
-        .plus(listOf('K').toMap(5))
-        .plus(listOf('J', 'X').toMap(8))
-        .plus(listOf('Q', 'Z').toMap(10))
+    return HashMap<Char, Int>().plus("AEIOULNRST".toMap(1))
+        .plus("DG".toMap(2))
+        .plus("BCMP".toMap(3))
+        .plus("FHVWY".toMap(4))
+        .plus("K".toMap(5))
+        .plus("JX".toMap(8))
+        .plus("QZ".toMap(10))
 }
 
 object ScrabbleScore {
@@ -23,6 +23,6 @@ object ScrabbleScore {
     }
 
     fun scoreWord(word: String, multiplier: Array<Int> = Array(word.length) { 1 }): Int {
-        return IntRange(0, word.length - 1).sumOf { scoreLetter(word.uppercase()[it]) * (multiplier[it]) }
+        return IntRange(0, word.length - 1).sumOf { scoreLetter(word.uppercase()[it]) * (multiplier[it] ?: 1) }
     }
 }
--- scrabble-score/src/test/kotlin/ScrabbleScoreTest.kt
+++ scrabble-score/src/test/kotlin/ScrabbleScoreTest.kt
@@ -9,24 +9,34 @@ class ScrabbleScoreTest(val input: String, val expectedOutput: Int) {
     companion object {
         @JvmStatic
         @Parameterized.Parameters(name = "{index}: scoreWord({0})={1}")
-        fun data() = listOf(
-                arrayOf("a", 1),
-                arrayOf("A", 1),
-                arrayOf("f", 4),
-                arrayOf("at", 2),
-                arrayOf("zoo", 12),
-                arrayOf("street", 6),
-                arrayOf("quirky", 22),
-                arrayOf("OxyphenButazone", 41),
-                arrayOf("pinata", 8),
-                arrayOf("", 0),
-                arrayOf("abcdefghijklmnopqrstuvwxyz", 87)
-        )
+        fun data() =
+            listOf(arrayOf("a", 1), arrayOf("A", 1), arrayOf("f", 4), arrayOf("at", 2), arrayOf("zoo", 12), arrayOf("street", 6), arrayOf("quirky", 22), arrayOf("OxyphenButazone", 41), arrayOf("pinata", 8), arrayOf("", 0), arrayOf("abcdefghijklmnopqrstuvwxyz", 87))
     }
 
     @Test
     fun test() {
         assertEquals(expectedOutput, ScrabbleScore.scoreWord(input))
     }
+
+    @Test
+    fun testDouble() {
+        assertEquals(174, ScrabbleScore.scoreWord("abcdefghijklmnopqrstuvwxyz", Array(26) { 2 }))
+    }
+
+    @Test
+    fun testTriple() {
+        assertEquals(261, ScrabbleScore.scoreWord("abcdefghijklmnopqrstuvwxyz", Array(26) { 3 }))
+    }
+
+    @Test
+    fun testLongMultiplier() {
+        assertEquals(87, ScrabbleScore.scoreWord("abcdefghijklmnopqrstuvwxyz", Array(27) { 1 }))
+    }
+
+    @Test
+    fun testShortMultiplier() {
+        assertEquals(91, ScrabbleScore.scoreWord("abcdefghijklmnopqrstuvwxyz", Array(2) { 2 }), "A & B doubled, with a short multiplier")
+    }
+
 
 }
