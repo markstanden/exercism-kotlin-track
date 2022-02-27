3d8479e1297b4d49199a0781bf8f5a50d90b107b
Mark Standen <mark@markstanden.dev>
2022-02-27 09:20:21 +0000

    added hamming

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,10 @@
 object Hamming {
 
+    private val mismatchedLength = IllegalArgumentException("left and right strands must be of equal length")
+
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+        if (leftStrand.length != rightStrand.length) throw mismatchedLength
+
+        return leftStrand.zip(rightStrand).count { it.first != it.second }
     }
 }
