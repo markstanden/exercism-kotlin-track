3d9305e8023f0ee97da2f61913a3c4cbd356ba6c
Mark Standen <mark@markstanden.dev>
2022-02-27 10:45:40 +0000

    completed matrix

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -1,10 +1,18 @@
 class Matrix(private val matrixAsString: String) {
+    private val matrix = matrixFromSpacesAndLineFeed(matrixAsString);
 
-    fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+    fun column(colNr: Int): List<Int> = IntRange(0, matrix.size - 1).map { matrix[it][colNr - 1] }
+
+    fun row(rowNr: Int): List<Int> = matrix[rowNr - 1]
+
+    fun matrixFromSpacesAndLineFeed(input: String): List<List<Int>> {
+        return input.split("\n")
+            .map {
+                it.split(" ")
+                    .map { str -> str.trim() }
+                    .filter { str -> str != "" }
+                    .map { str -> str.toInt() }
+            }
     }
 
-    fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
-    }
 }
