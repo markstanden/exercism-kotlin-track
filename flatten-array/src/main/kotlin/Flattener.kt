26c0bd7343b9d4e34f6a2b621c36834e0ec9668e
Mark Standen <mark@markstanden.dev>
2022-03-01 04:40:27 +0000

    needed to return a list so flatmap doesn't complain

--- flatten-array/src/main/kotlin/Flattener.kt
+++ flatten-array/src/main/kotlin/Flattener.kt
@@ -1,13 +1,6 @@
-import java.util.stream.Stream
-
 object Flattener {
-    fun flattenIt(source: Collection<Any?>): List<Any> {
-        return source.filterNotNull()
-            .map { if (it is Collection<Any?>) flatten(it.flatMap { x->x }) else it }
-    }
-
     fun flatten(source: Collection<Any?>): List<Any> {
-        val seq = flattenIt(source)
-        return seq.toList().
+        return source.filterNotNull()
+                     .flatMap { if (it is Collection<*>) flatten(it) else listOf(it) }
     }
 }
