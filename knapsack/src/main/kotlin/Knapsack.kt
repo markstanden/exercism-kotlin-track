558db608dc4ad51171196ece01aecced15dfd310
Mark Standen <mark@markstanden.dev>
2022-03-02 10:29:17 +0000

    completed knapsack challenge

--- knapsack/src/main/kotlin/Knapsack.kt
+++ knapsack/src/main/kotlin/Knapsack.kt
@@ -1,5 +1,35 @@
 data class Item(val weight: Int, val value: Int)
 
 fun knapsack(maximumWeight: Int, items: List<Item>): Int {
-    TODO("Implement this function to complete the task")
+
+    fun addItem(currentWeight: Int, remainingItems: List<Item>, currentValue: Int): Int {
+        val itemsThatFit = remainingItems.filter { it.weight <= maximumWeight - currentWeight }
+        if (itemsThatFit.isEmpty() or (currentWeight >= maximumWeight)) {
+            return currentValue
+        }
+
+        return itemsThatFit.maxOf {
+            addItem(currentWeight.plus(it.weight), remainingItems.without(it),
+                    currentValue.plus(it.value))
+        }
+    }
+    return addItem(0, items, 0)
+}
+
+/**
+ * Filters a list and returns a new list without the first element matching the supplied predicate.
+ * @param      item      The Item to filter
+ * @returns    List with the first structurally equal occurrence removed.
+ */
+fun <T> List<T>.without(item: T) =
+        this.withoutMatching { it == item }
+
+/**
+ * Filters a list and returns a new list without the first element matching the supplied predicate.
+ * @param      predicate   The predicate to match the first item to exclude
+ * @returns    List with only the first matching occurrence removed.
+ */
+fun <T> List<T>.withoutMatching(predicate: (T) -> Boolean): List<T> {
+    val first = this.firstOrNull() { predicate(it) }
+    return this.filter { first !== it }
 }
--- knapsack/src/test/kotlin/KnapsackTest.kt
+++ knapsack/src/test/kotlin/KnapsackTest.kt
@@ -4,83 +4,83 @@ import kotlin.test.assertEquals
 class KnapsackTest {
 
     @Test
-    fun `no items`() = assertEquals(knapsack(100, listOf()), 0)
+    fun `no items`() = assertEquals(0, knapsack(100, listOf()))
 
     @Test
     fun `one item, too heavy`() {
         val items = listOf(Item(100, 1))
-        assertEquals(knapsack(10, items), 0)
+        assertEquals(0, knapsack(10, items))
     }
 
     @Test
     fun `five items (cannot be greedy by weight)`() {
         val items = listOf(
-            Item(2, 5),
-            Item(2, 5),
-            Item(2, 5),
-            Item(2, 5),
-            Item(10, 21)
+                Item(2, 5),
+                Item(2, 5),
+                Item(2, 5),
+                Item(2, 5),
+                Item(10, 21)
         )
-        assertEquals(knapsack(10, items), 21)
+        assertEquals(21, knapsack(10, items))
     }
 
     @Test
     fun `five items (cannot be greedy by value)`() {
         val items = listOf(
-            Item(2, 20),
-            Item(2, 20),
-            Item(2, 20),
-            Item(2, 20),
-            Item(10, 50)
+                Item(2, 20),
+                Item(2, 20),
+                Item(2, 20),
+                Item(2, 20),
+                Item(10, 50)
         )
-        assertEquals(knapsack(10, items), 80)
+        assertEquals(80, knapsack(10, items))
     }
 
     @Test
     fun `example knapsack`() {
         val items = listOf(
-            Item(5, 10),
-            Item(4, 40),
-            Item(6, 30),
-            Item(4, 50)
+                Item(5, 10),
+                Item(4, 40),
+                Item(6, 30),
+                Item(4, 50)
         )
-        assertEquals(knapsack(10, items), 90)
+        assertEquals(90, knapsack(10, items))
     }
 
     @Test
     fun `8 items`() {
         val items = listOf(
-            Item(25, 350),
-            Item(35, 400),
-            Item(45, 450),
-            Item(5, 20),
-            Item(25, 70),
-            Item(3, 8),
-            Item(2, 5),
-            Item(2, 5)
+                Item(25, 350),
+                Item(35, 400),
+                Item(45, 450),
+                Item(5, 20),
+                Item(25, 70),
+                Item(3, 8),
+                Item(2, 5),
+                Item(2, 5)
         )
-        assertEquals(knapsack(104, items), 900)
+        assertEquals(900, knapsack(104, items))
     }
 
     @Test
     fun `15 items`() {
         val items = listOf(
-            Item(70, 135),
-            Item(73, 139),
-            Item(77, 149),
-            Item(80, 150),
-            Item(82, 156),
-            Item(87, 163),
-            Item(90, 173),
-            Item(94, 184),
-            Item(98, 192),
-            Item(106, 201),
-            Item(110, 210),
-            Item(113, 214),
-            Item(115, 221),
-            Item(118, 229),
-            Item(120, 240)
+                Item(70, 135),
+                Item(73, 139),
+                Item(77, 149),
+                Item(80, 150),
+                Item(82, 156),
+                Item(87, 163),
+                Item(90, 173),
+                Item(94, 184),
+                Item(98, 192),
+                Item(106, 201),
+                Item(110, 210),
+                Item(113, 214),
+                Item(115, 221),
+                Item(118, 229),
+                Item(120, 240)
         )
-        assertEquals(knapsack(750, items), 1458)
+        assertEquals(1458, knapsack(750, items))
     }
 }
