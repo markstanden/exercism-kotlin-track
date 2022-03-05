1ecd3a89b44fdc13199a8e106a1e2e6ca191751a
Mark Standen <mark@markstanden.dev>
2022-03-05 05:42:10 +0000

    completed darts

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,12 @@
+import kotlin.math.hypot
+
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
-    }
+    fun score(x: Number, y: Number) =
+        when (hypot(x.toDouble(), y.toDouble())) {
+            in 0.0..1.0 -> 10
+            in 1.0..5.0 -> 5
+            in 5.0..10.0 -> 1
+            else -> 0
+        }
 }
