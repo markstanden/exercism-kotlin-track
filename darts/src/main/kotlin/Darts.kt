9a53c8823de4c47ce0f799f514874cffe701bf93
Mark Standen <mark@markstanden.dev>
2022-03-05 05:47:04 +0000

    added consts to darts

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,12 +1,19 @@
 import kotlin.math.hypot
 
+const val INNER_RADIUS = 1.0
+const val MIDDLE_RADIUS = 5.0
+const val OUTER_RADIUS = 10.0
+const val INNER_POINTS = 10
+const val MIDDLE_POINTS = 5
+const val OUTER_POINTS = 1
+
 object Darts {
 
     fun score(x: Number, y: Number) =
         when (hypot(x.toDouble(), y.toDouble())) {
-            in 0.0..1.0 -> 10
-            in 1.0..5.0 -> 5
-            in 5.0..10.0 -> 1
+            in 0.0..INNER_RADIUS -> INNER_POINTS
+            in INNER_RADIUS..MIDDLE_RADIUS -> MIDDLE_POINTS
+            in MIDDLE_RADIUS..OUTER_RADIUS -> OUTER_POINTS
             else -> 0
         }
 }
