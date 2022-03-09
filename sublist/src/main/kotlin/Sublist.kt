40f25d09490152a810dba96fd638d18819f9e4b1
Mark Standen <mark@markstanden.dev>
2022-03-09 04:34:00 +0000

    completed sublist

--- sublist/src/main/kotlin/Sublist.kt
+++ sublist/src/main/kotlin/Sublist.kt
@@ -3,3 +3,22 @@ enum class Relationship {
     EQUAL, SUBLIST, SUPERLIST, UNEQUAL
 
 }
+
+fun <T> List<T>.relationshipTo(that: List<T>): Relationship {
+    if (that == this) return Relationship.EQUAL
+    if (this.isSublistOf(that)) return Relationship.SUBLIST
+    if (that.isSublistOf(this)) return Relationship.SUPERLIST
+    return Relationship.UNEQUAL
+}
+
+/**
+ * Checks whether the current list is
+ * contained sequentially within the provided list
+ */
+fun <T> List<T>.isSublistOf(bigList: List<T>): Boolean {
+    return bigList.indices.any { bigIndex ->
+        this.indices.all {
+            this[it] == bigList.getOrNull(it + bigIndex)
+        }
+    }
+}
